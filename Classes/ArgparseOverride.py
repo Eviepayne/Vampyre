84e8e80a82bf43373f3cbf16e75e3202e1dabd80
vlad <vlad@vladevpayne.xyz>
2023-08-18 18:30:27 +0000

    Fixed exit bug, thanks argparse

--- Classes/ArgparseOverride.py
+++ Classes/ArgparseOverride.py
@@ -4,6 +4,13 @@ class ArgumentParser(argparse.ArgumentParser):
     def error(self, message):
         raise ValueError(message)
 
+    def exit(self, status=0, message=None):
+        if message:
+            self._print_message(message, _sys.stderr)
+
+    def _print_message(self, message, file=None):
+        pass
+
 # parser = ArgumentParser(description='My custom error handler')
 # parser.add_argument('--foo', required=True)
 
