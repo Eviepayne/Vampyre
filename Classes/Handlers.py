a4a04e732a99a748b4e5a13197707095c140d06b
vlad <vlad@vladevpayne.xyz>
2023-08-18 18:30:40 +0000

    Polished messagesweep no bugs

--- Classes/Handlers.py
+++ Classes/Handlers.py
@@ -1,4 +1,4 @@
-import sqlite3, os, logging, argparse, time
+import sqlite3, os, logging, argparse, time, threading, textwrap
 from pyrogram import filters, enums, types, errors
 from Classes.Keyboard import Keyboard
 from Classes.Callback import Callback
@@ -7,6 +7,7 @@ from Classes.Manager import HandlerManager
 
 lastupdate = {}
 lastusersindex = {}
+handler_lock = threading.Lock()
 
 class Handlers():
 
@@ -16,21 +17,20 @@ class Handlers():
   # Message Sweep =====================================================================================
     def messagesweep(bot, message):
         """Deletes messages
-
-        Args:
-            bot (bot): bot class
-            message (message): message object
+        bot = bot object
+        message = content passed from handler
         """
 
      # Handle CallbackQueries
-        if type(message) is types.CallbackQuery and Handlers.is_admin(bot, message.message.chat.id, message.from_user.id):
-            inline_keyboard = message.message.reply_markup.inline_keyboard
-            callback_actions = {button.text: button.callback_data for row in inline_keyboard for button in row if hasattr(button, 'text') and hasattr(button, 'callback_data')}
+        if type(message) is types.CallbackQuery and not Handlers.is_admin(bot, message.message.chat.id, message.from_user.id):
+            return
+        elif type(message) is types.CallbackQuery:
+            callback_actions = {button.text: button.callback_data for row in message.message.reply_markup.inline_keyboard for button in row if hasattr(button, 'text') and hasattr(button, 'callback_data')}
             selectedguid = message.data
-            data, handler = Callback.ReadCallback(selectedguid)
-            deletelist = data['deletelist']
             for guid in callback_actions.values():
                 data, handler = Callback.ReadCallback(guid)
+                if guid == selectedguid:
+                    deletelist = data['deletelist']
                 Callback.DestroyCallback(bot, guid, handler)
             if selectedguid == callback_actions['Delete Messages']:
                 deletelist.append(message.message.id)
@@ -38,59 +38,64 @@ class Handlers():
                     bot.delete_messages(message.message.chat.id, deletelist)
                 except Exception as e:
                     bot.send_message(message.message.chat.id, f"We failed to delete the following messages:\n{deletelist}\nThis was the issue: {e}")
-
-            if selectedguid == callback_actions['Cancel Operation']:
+            else:
                 deletelist.clear()
                 deletelist.append(message.message.id)
                 bot.delete_messages(message.message.chat.id, deletelist)
-
             return
+        
+     # Delete original message   
+        bot.delete_messages(message.chat.id, message.id)
 
      # Return if not admin
         if not Handlers.is_admin(bot, message.chat.id, message.from_user.id):
+            bot.delete_messages(message.chat.id, message.id)
             return
-
-        bot.delete_messages(message.chat.id, message.id) # I think this caused the bug.
-        # by the time we go to calculate the first message is empty.
-        # If this isnt it, I also changed the current >= target so it only checks if its greater.
 
      # Create argparse and overwrite
         parser = ArgumentParser(description='Delete messages in this group')
 
-        helpdialog = """
-      Delete messages in this group.
-      To delete a target message, reply to the message with command: /del 1
-    
-        /del [-h]                         
-        - Help Dialog
-        
-        /del number (no replied message)  
-        - Delete the number of messages above
-        
-        /del (replying to message)       
-        - Delete all messages up to (including) the replied message.
-        
-        /del number (replying to message)
-        - Delete the number of messages after (including) the replied message.
-
-      positional arguments:
-      number      The number of messages to delete
-
-      optional arguments:
-      -h, --help  show this help message
+        helpdialog = textwrap.dedent(
         """
+        **Delete Messages in This Group**
+
+        To delete a target message, reply to the message with command: `/del 1`.
+
+        **Commands**
+
+        `/del` [-h | help] 
+        Display the __Help Dialog__.
+
+        `/del number` (when not replying to a message)
+        Delete the specified number of messages above.
+
+        `/del` (when replying to a message)
+        Delete all messages up to and including the replied message.
+
+        `/del number` (when replying to a message)
+        Delete the specified number of messages after, including the replied message.
+
+        **Arguments**
+
+        --Positional Arguments:--
+        `number`  
+        The number of messages to delete.
+
+        --Optional Arguments:--
+        `-h`, `--help`  
+        Show the help message.
+        """)
         parser.add_argument('number', type=int, nargs='?', default=None, help='The number of messages to delete')
         command = message.text.split()
         try:
             args = parser.parse_args(command[1:])
         except Exception as e:
-            bot.send_message(message.chat.id, f"{e}\n\nMake sure you're using this command correctly\n{helpdialog}")
+            bot.send_message(message.chat.id, f"Invalid Argument\n{helpdialog}")
             return
-
 
      # If no reply and no number
         if not message.reply_to_message and args.number is None:
-            bot.send_message(message.chat.id, f"```{helpdialog}```")
+            bot.send_message(message.chat.id, helpdialog)
             return
 
      # Bot sending original message to let user know it's working.
@@ -108,10 +113,11 @@ class Handlers():
                 currentmessage = bot.get_messages(message.chat.id, currentmessageid)
                 if currentmessage.empty:
                     sanitycheck += 1
+                    if sanitycheck == 1000:
+                        # TODO - Benchmark the sanity check to ensure we aren't waiting too long
+                        break
                     targetmessageid -= 1
                     currentmessageid -= 1
-                    if sanitycheck == 101:
-                        break
                     continue
                 deletelist.append(currentmessageid)
                 sanitycheck = 0
@@ -120,10 +126,9 @@ class Handlers():
      # If has reply and no number
         if message.reply_to_message and args.number is None:
             currentmessageid = message.id
-            targetmessageid = message.reply_to_message
             while currentmessageid >= message.reply_to_message.id:
                 currentmessage = bot.get_messages(message.chat.id, currentmessageid)
-                if currentmessage.empty:
+                if currentmessage.empty: # We need to do this since we're appending messages to a list and providing a number to the user
                     currentmessageid -= 1
                     continue
                 deletelist.append(currentmessageid)
@@ -137,10 +142,10 @@ class Handlers():
                 currentmessage = bot.get_messages(message.reply_to_message.chat.id, currentmessageid)
                 if currentmessage.empty:
                     sanitycheck += 1
-                    targetmessageid -= 1
-                    currentmessageid -= 1
                     if sanitycheck == 100:
                         break
+                    targetmessageid -= 1
+                    currentmessageid -= 1
                     continue
                 deletelist.append(currentmessageid)
                 sanitycheck = 0
@@ -152,32 +157,37 @@ class Handlers():
         Delete = Keyboard.create_button(text="Delete Messages",callback_data=guiddelete)
         Cancel = Keyboard.create_button(text="Cancel Operation",callback_data=guidcancel)
         keyboard = Keyboard.create_keyboard(Delete, Cancel)
-        bot.edit_message_text(statusmessage.chat.id, statusmessage.id, f"Confirm deleting {len(deletelist)} messages", reply_markup=keyboard)
-        # Timeout for callback
+        bot.edit_message_text(statusmessage.chat.id, statusmessage.id, f"Confirm deleting {len(deletelist)} messages", reply_markup=keyboard)        
+        threading.Thread(target=Handlers.messagesweepcleanup, args=(bot, statusmessage, guiddelete, guidcancel)).start()
+
+    # Cleanup for messagesweep
+    def messagesweepcleanup(bot, statusmessage, guiddelete, guidcancel):
+        """ Cleans up after messagesweep
+        bot = bot object
+        statusmessage = message object for the bots response
+        guiddelete = if for the delete handler
+        guidcancel = id for the cancel handler
+        """
         time.sleep(30)
-        manager = HandlerManager()
-        handlerdelete = manager.get_handler(guiddelete)
-        handlercancel = manager.get_handler(guidcancel)
-        if handlerdelete:
-            Callback.DestroyCallback(bot, guiddelete, handlerdelete)
-            Callback.DestroyCallback(bot, guidcancel, handlercancel)
+        with handler_lock:
+            manager = HandlerManager()
+            handlerdelete = manager.get_handler(guiddelete)
+            handlercancel = manager.get_handler(guidcancel)
+            if handlerdelete:
+                Callback.DestroyCallback(bot, guiddelete, handlerdelete)
+                Callback.DestroyCallback(bot, guidcancel, handlercancel)
             bot.delete_messages(statusmessage.chat.id, statusmessage.id)
-        
 
   # is_admin =====================================================================================
     def is_admin(bot, chatid, userid):
-        """Returns true if user is admin
-
-        Args:
-            bot (bot): bot class
-            chatid (int): Unique Identifier for the chat
-            userid (int): Unique Identifier for the user
-
-        Returns:
-            Bool: Returns True if the user is an administrator or owner
+        """Returns true if chatid is admin of userid
+        bot = bot object
+        chatid = chat id
+        userid = user id
         """
         try:
             ChatMember = bot.get_chat_member(chatid, userid)
+
         except errors.FloodWait as e:
             bot.send_message(chatid, "We are being rate limited. Please wait.")
             return False
