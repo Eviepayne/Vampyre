83fd05c6025ebb209ec3f20a39aa1e7d829e5764
vlad <vlad@vladevpayne.xyz>
2023-09-07 00:30:22 +0000

    added comments for clarity

--- Classes/Keyboard.py
+++ Classes/Keyboard.py
@@ -6,7 +6,10 @@ class Keyboard:
         return InlineKeyboardButton(**kwargs)
 
     def create_keyboard(*buttons):
+        # get the number of buttons I created
         buttonscount = len(buttons)
+
+        # determine the number of columns based on the number of buttons
         if buttonscount == 1:
             cols = 1
         elif buttonscount == 2:
@@ -22,18 +25,25 @@ class Keyboard:
         else:
             cols = 3
 
+        # calculate the number of rows based on the number of columns and buttons
         rows = (buttonscount + cols - 1) // cols
+        # instantiate keyboard object
         keyboard = []
 
+        # Set starter location in the grid
         button_index = 0
+        # Every Row, Create the row object, and create each column
         for row in range(rows):
             row_buttons = []
+            # Each column if the index is less than how many buttons we have
+            # create a button for the cell
             for col in range(cols):
-                if button_index < buttonscount: # TODO - I might be able to remove this if check. Test it.
-                    row_buttons.append(buttons[button_index]) # Append the existing button object
-                    button_index += 1 # TODO - I might be able to remove this as well, if I can remove the above instantiation
+                if button_index < buttonscount:
+                    row_buttons.append(buttons[button_index])
+                    button_index += 1
                 else:
                     break
+            # when the row is finished, add it to the keyboard
             keyboard.append(row_buttons)
-
+        # When keyboard is done, return it
         return InlineKeyboardMarkup(keyboard)
